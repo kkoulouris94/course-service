cb6214430d7407f11548d2f3e549622254cc1c70
Konstantinos Koulouris <kkoulouris@psp.gr>
2023-05-08 14:04:46 +0300

    master: Create courses table

---
+++ database/migrations/2023_05_08_110156_create_courses_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('courses', function (Blueprint $table) {
+            $table->id();
+            $table->string('title')->unique();
+            $table->text('description');
+            $table->enum('status', ['Published', 'Pending']);
+            $table->boolean('is_premium');
+            $table->timestamp('created_at')->default(date('Y-m-d H:i:s'));
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('courses');
+    }
+};
