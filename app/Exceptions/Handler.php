da17b67908e2db0baebf841ffacb082155c7012c
Konstantinos Koulouris <kkoulouris@psp.gr>
2023-05-08 14:53:54 +0300

    master: Return only JSON responses

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,15 +2,20 @@
 
 namespace App\Exceptions;
 
+use App\Traits\ApiResponse;
 use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Validation\ValidationException;
 use Laravel\Lumen\Exceptions\Handler as ExceptionHandler;
+use Symfony\Component\HttpFoundation\Exception\BadRequestException;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\HttpException;
 use Throwable;
 
 class Handler extends ExceptionHandler
 {
+    use ApiResponse;
+
     /**
      * A list of the exception types that should not be reported.
      *
@@ -49,6 +54,31 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Throwable $exception)
     {
-        return parent::render($request, $exception);
+        if ($exception instanceof HttpException)
+        {
+            $code = $exception->getStatusCode();
+            $message = Response::$statusTexts[$code];
+
+            return $this->errorResponse($message, $code);
+        }
+
+        if ($exception instanceof ModelNotFoundException)
+        {
+            $model = strtolower(class_basename($exception->getModel()));
+            return $this->errorResponse("{$model} with given id not found", Response::HTTP_NOT_FOUND);
+        }
+
+        if ($exception instanceof ValidationException)
+        {
+            $errors = $exception->validator->errors()->getMessages();
+            return $this->errorResponse($errors, Response::HTTP_UNPROCESSABLE_ENTITY);
+        }
+
+        if ($exception instanceof BadRequestException)
+        {
+            return $this->errorResponse($exception->getMessage(), Response::HTTP_BAD_REQUEST);
+        }
+
+        return $this->errorResponse($exception->getMessage() ?? 'Something went wrong while processing your request', Response::HTTP_INTERNAL_SERVER_ERROR);
     }
 }
